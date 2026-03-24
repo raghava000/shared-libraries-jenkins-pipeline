0e9bbfca494c221a318d8de6af343eefec2b1b41
raghava000 <raghava000@gmail.com>
2026-03-24 14:41:30 +0530

    Add shared Docker build function

--- vars/buildDocker.groovy
+++ vars/buildDocker.groovy
@@ -1,1 +1,6 @@
-
+// vars/buildDocker.groovy
+def call(String imageName) {
+    echo "--- STARTING SHARED DOCKER BUILD FOR ${imageName} ---"
+    sh "docker build -t ${imageName}:${env.BUILD_NUMBER} ."
+    echo "--- BUILD COMPLETE ---"
+}
